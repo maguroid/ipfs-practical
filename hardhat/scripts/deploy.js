b8de6bb40c6bf6fbb4e69754daf4b04f1f99a4fb
maguro <maguro.devlife@gmail.com>
2022-06-26 13:13:53 +0900

    hardhat contracts implementaion

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,21 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  const metadataURL = "ipfs://QmQBHarz2WFczTjz5GnhjHrbUPDnB48W5BM2v2h6HbE1rZ";
+
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+
+  await deployedLW3PunksContract.deployed();
+
+  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
